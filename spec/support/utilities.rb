740e3d5522927eb2442a03cc2b236beac572cb6b
test <test@gmail.com>
2013-12-04 11:14:41 +0530

    Chapter 5 end

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,4 @@
-module ApplicationHelper
-
-   def full_title(page_title)
+def full_title(page_title)
       base_title = "Ruby on Rails Tutorial Sample App"
       if page_title.empty?
          base_title
@@ -8,5 +6,3 @@ module ApplicationHelper
          "#{base_title} | #{page_title}"
       end
     end
-        
-end
